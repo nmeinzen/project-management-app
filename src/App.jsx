5f6911d217d9096a00d807d770bed70997ebc7e2
nmeinzen <44786549+nmeinzen@users.noreply.github.com>
2024-01-30 12:29:12 +0000

    Handling creating new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({selectedProjectId: undefined, projectsArray: []});
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return{
+        ...prevState,
+        selectedProjectId: null,
+      }
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  }
+  else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
-    <main class="h-screen my-8">
-      <ProjectsSidebar />
+    <main class="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
