3a762efcac02a2fb3a7064492f8fe2828e07aac7
nmeinzen <44786549+nmeinzen@users.noreply.github.com>
2024-01-30 12:40:03 +0000

    Adding 'add event' functionality

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,19 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const newProject = {projectData, id: Math.random()}
+      return {
+        ...prevState,
+        projects: [...prevState.projectsArray, newProject]
+      }
+    })
+  }
+
   let content;
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject}/>
   }
   else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
